126457b2dcd7970050a9a6eaa92eaa45067807f3
yutakaki <yu.takaki2000@gmail.com>
2022-03-13 23:53:16 +0900

    added 2 buttons and centered it

---
+++ pages/login.tsx
@@ -0,0 +1,10 @@
+import { TextField } from '@mui/material'
+import React from 'react'
+
+const login = () => {
+  return (
+    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+  )
+}
+
+export default login
---
+++ pages/user.tsx
@@ -0,0 +1,11 @@
+import { Button, Container } from '@mui/material'
+import Link from 'next/link'
+import React from 'react'
+
+const user = () => {
+  return (
+    <h1></h1>
+  )
+}
+
+export default user
